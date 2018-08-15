92cdbd30c2eb1e2465c4932fa511c29d32426b0e
Simon Ashdown <swashdown@gmail.com>
2018-08-15 11:55:35 +0100

    refactor out pump actions to a separate call.

--- actions.py
+++ actions.py
@@ -6,27 +6,42 @@ from temperature import read_temp
 
 MONITOR_BOILER_SWITCHOFF_DELAY = 1
 
-ENERGENIE_SOCKET_NUMBER = 2
+ENERGENIE_BOILER_SOCKET_NUMBER = 2
+ENERGENIE_PUMP_SOCKET_NUMBER = 2
 
 from gpiozero import Energenie
 
-socket = Energenie(ENERGENIE_SOCKET_NUMBER)
+boiler_socket = Energenie(ENERGENIE_BOILER_SOCKET_NUMBER)
+pump_socket = Energenie(ENERGENIE_PUMP_SOCKET_NUMBER)
+
 
 def wait(minutes):
     sleep(minutes * 60)
 
-def switch_on_boiler():
 
-    socket.on()
+def switch_on_boiler():
+    boiler_socket.on()
 
     logging.debug("Boiler On")
 
+
+def switch_on_pump():
+    pump_socket.on()
+
+    logging.debug("Pump On")
+
+
 def switch_off_boiler():
-    logging.debug("Boiler Off")
-    socket.off()
+    logging.debug("Pump Off")
+    boiler_socket.off()
+
+
+def switch_off_pump():
+    logging.debug("Pump Off")
+    pump_socket.off()
+
 
 def raise_temperature_for(target_temperature, target_minutes=0):
-
     raise_temperature(target_temperature)
 
     logging.debug("Holding at {} for {} minutes".format(target_temperature, target_minutes))
@@ -38,8 +53,8 @@ def raise_temperature_for(target_temperature, target_minutes=0):
         raise_temperature(target_temperature)
     logging.debug("Held at {} for {} seconds ".format(target_temperature, (datetime.now() - start).seconds))
 
-def raise_temperature(target_temperature):
 
+def raise_temperature(target_temperature):
     while (read_temp() < target_temperature):
         switch_on_boiler()
         logging.debug("Boiler On.  Waiting {} secs to rise to  {}. Current temp {}".format(
@@ -47,15 +62,15 @@ def raise_temperature(target_temperature):
             target_temperature, read_temp()))
         sleep(MONITOR_BOILER_SWITCHOFF_DELAY)
     switch_off_boiler()
-    logging.debug("Target temperature {} reached.  Now {}C".format( target_temperature, read_temp()))
+    logging.debug("Target temperature {} reached.  Now {}C".format(target_temperature, read_temp()))
 
 
 def wait_until_temperature_has_fallen_to(target_temperature):
-    while (read_temp() > target_temperature):
-        switch_on_boiler()
+    while (read_temp() > target_temperature): # alert if temperature does not drop within 1 minute
+        switch_on_pump()
         logging.debug("Waiting to fall to {}. Current temp {}".format(target_temperature, read_temp()))
         sleep(10)
-    switch_off_boiler()
+    switch_off_pump()
     logging.debug("Target temperature reached")
 
 
@@ -70,15 +85,13 @@ def message(action):
     sc.api_call(
         "chat.postMessage",
         channel="updates",
-        text="{}> {}".format(now.strftime("%H:%M:%S") , action)
+        text="{}> {}".format(now.strftime("%H:%M:%S"), action)
     )
     print(action)
+
 
 def manual_action(action):
     message("ACTION REQUIRED: {}".format(action))
     input(action)
     message("COMPLETE: {}".format(action))
 
-# Cool / Heat to target temperature and then keep at that temperature for specified minutes
-def maintain(temperature, minutes):
-    pass
