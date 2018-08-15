7cdfcd79bef531d9c811bbd2b291158563d7262f
Simon Ashdown <swashdown@gmail.com>
2018-08-15 14:21:17 +0100

    use fixture for pump

--- tests/test_pump_actions.py
+++ tests/test_pump_actions.py
@@ -2,13 +2,15 @@
 import logging
 
 import mock as mock
-
-
+from pytest import fixture
 
 logging.basicConfig(level=logging.DEBUG)
 
 from actions import wait_until_temperature_has_fallen_to
 
+@fixture
+def pump():
+    return mock.Mock()
 
 def test_pump_remains_off_below_target_temp():
     mock_temperature = mock.Mock()
@@ -24,16 +26,15 @@ def test_pump_remains_off_below_target_temp():
     pump_mock.off.assert_called_once_with()
 
 
-def test_pump_switches_on_above_target_temp():
+def test_pump_switches_on_above_target_temp(pump):
     mock_temperature = mock.Mock()
     mock_temperature.side_effect=[11,11,9,9]
 
-    pump_mock = mock.Mock()
     wait_until_temperature_has_fallen_to(target_temperature=10,
-                                         pump=pump_mock,
+                                         pump=pump,
                                          read_delay=0,
                                          temperature_fn=mock_temperature)
 
 
-    pump_mock.off.assert_called_once_with()
-    pump_mock.on.assert_called_once_with()
+    pump.off.assert_called_once_with()
+    pump.on.assert_called_once_with()
